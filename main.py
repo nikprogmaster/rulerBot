c7eddf05459a6bdb68c6e9f89e9d20ea602065ec
Nikita <nikitakandyba@yandex.ru>
2021-09-23 23:10:01 +0300

    non stop bot cycle

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import telebot
 from PhraseSender import PhraseSender
 from telebot import types
 from threading import Thread
+from time import sleep
 
-bot = telebot.TeleBot('2049393246:AAEn8VcXip1p0O-k8sgYMakhFJue_FGEcwo')
+BOT_TOKEN = '2049393246:AAEn8VcXip1p0O-k8sgYMakhFJue_FGEcwo'
+BOT_INTERVAL = 3
+BOT_TIMEOUT = 30
+bot = None
 maintainers = []
 all_phrases = []
 active_maintainers = {}
@@ -69,75 +73,97 @@ def find_maintainer_by_id(identificator):
             return m
 
 
-@bot.message_handler(commands=['start'], content_types=['text'])
-def send_welcome(message):
-    if message.chat.username in maintainers:
-        bot.send_message(message.chat.id, "Привет, " + message.chat.first_name + "!", reply_markup=get_begining_keyboard())
-    else:
-        user_markup = telebot.types.ReplyKeyboardMarkup(True)
-        user_markup.row(start_game)
-        bot.send_message(message.chat.id, "Добро пожаловать!", reply_markup=user_markup)
+def bot_polling():
+    global bot
+    print("Starting bot polling now")
+    while True:
+        try:
+            print("New bot instance started")
+            bot = telebot.TeleBot(BOT_TOKEN)
+            init()
+            bot_actions()
+            bot.polling(none_stop=True, interval=BOT_INTERVAL, timeout=BOT_TIMEOUT)
+        except Exception as ex:
+            print("Bot polling failed, restarting in {}sec. Error:\n{}".format(BOT_TIMEOUT, ex))
+            bot.stop_polling()
+            sleep(BOT_TIMEOUT)
+        else:
+            bot.stop_polling()
+            print("Bot polling loop finished")
+            break
 
 
-@bot.message_handler(content_types=['text'])
-def bot_managering(message):
-    if message.chat.type == 'private':
-        maintainer = active_maintainers.get(message.chat.username)
-        if message.text == start_new_game:
-            if maintainer is not None and maintainer.is_game_started:
-                bot.send_message(message.chat.id, "Ты уже играешь!", reply_markup=get_before_game_keyboard())
-            elif len(active_maintainers) >= max_maintainers:
-                bot.send_message(message.chat.id,
-                                 "Привет! Прости, сейчас много людей пользуется ботом. Подожди немного")
-            else:
-                maintainer = PhraseSender(maintainers.index(message.chat.username),
-                                          message.chat.username)
-                active_maintainers[message.chat.username] = maintainer
-                maintainer.is_game_started = True
-                bot.send_message(message.chat.id, 'Введите размер группы. Ее размер должен быть от 2 до 20',
-                                 reply_markup=get_before_game_keyboard())
-        elif maintainer is not None and message.text == continue_game:
-            if maintainer.group_number == 0:
-                bot.send_message(message.chat.id, 'Вы не ввели размер группы!', reply_markup=get_before_game_keyboard())
-            else:
-                bot.send_message(message.chat.id,
-                                 'Новая игра! Ваш id = ' + str(maintainer.uniq_id) + '. Сообщите его участникам',
-                                 reply_markup=get_pre_start_keyboard())
-        elif message.text == participants_number:
-            bot.send_message(message.chat.id, 'Сейчас ' + str(len(maintainer.participants)) + maintainer.get_correct_ending(),
-                             reply_markup=get_pre_start_keyboard())
-        elif message.text == start_playing:
-            bot.send_message(message.chat.id, 'Игра началась', reply_markup=get_game_keyboard())
-            Thread(target=maintainer.give_phrases(all_phrases, bot)).start()
-        elif message.text == start_game:
-            bot.send_message(message.chat.id, 'Введите id игры')
-        elif message.text == exit and maintainer is not None:
-            maintainer.is_game_started = False
-            active_maintainers.pop(maintainer.maintainer_username)
-            bot.send_message(message.chat.id, 'Игра окончена', reply_markup=get_begining_keyboard())
-        elif message.text == back:
-            bot.send_message(message.chat.id, 'Введите размер группы. Ее размер должен быть от 2 до 20',
-                             reply_markup=get_before_game_keyboard())
-        elif message.text.isdigit():
-            if maintainer is not None and message.chat.username == maintainer.maintainer_username:
-                if int(message.text) in range(2, 21):
-                    maintainer.group_number = int(message.text)
+def bot_actions():
+    @bot.message_handler(commands=['start'], content_types=['text'])
+    def send_welcome(message):
+        if message.chat.username in maintainers:
+            bot.send_message(message.chat.id, "Привет, " + message.chat.first_name + "!",
+                             reply_markup=get_begining_keyboard())
+        else:
+            user_markup = telebot.types.ReplyKeyboardMarkup(True)
+            user_markup.row(start_game)
+            bot.send_message(message.chat.id, "Добро пожаловать!", reply_markup=user_markup)
+
+    @bot.message_handler(content_types=['text'])
+    def bot_managering(message):
+        if message.chat.type == 'private':
+            maintainer = active_maintainers.get(message.chat.username)
+            if message.text == start_new_game:
+                if maintainer is not None and maintainer.is_game_started:
+                    bot.send_message(message.chat.id, "Ты уже играешь!", reply_markup=get_before_game_keyboard())
+                elif len(active_maintainers) >= max_maintainers:
                     bot.send_message(message.chat.id,
-                                     'Отлично! Размер группы ' + message.text + '. Максимальное количество участников для такого размера - ' + str(
-                                         20 * int(message.text)) + '. Если их будет больше, бот автоматически увеличит размер группы', reply_markup=get_before_game_keyboard_with_back())
+                                     "Привет! Прости, сейчас много людей пользуется ботом. Подожди немного")
                 else:
+                    maintainer = PhraseSender(maintainers.index(message.chat.username),
+                                              message.chat.username)
+                    active_maintainers[message.chat.username] = maintainer
+                    maintainer.is_game_started = True
                     bot.send_message(message.chat.id, 'Введите размер группы. Ее размер должен быть от 2 до 20',
                                      reply_markup=get_before_game_keyboard())
-            else:
-                maintainer = find_maintainer_by_id(int(message.text))
-                if maintainer is not None and maintainer.is_game_started is True:
-                    maintainer.add_participant(message.chat.id)
-                    bot.send_message(message.chat.id, 'Поздравляю, ты в игре!')
+            elif maintainer is not None and message.text == continue_game:
+                if maintainer.group_number == 0:
+                    bot.send_message(message.chat.id, 'Вы не ввели размер группы!',
+                                     reply_markup=get_before_game_keyboard())
                 else:
-                    bot.send_message(message.chat.id, 'Прости, игра еще не началась :(')
+                    bot.send_message(message.chat.id,
+                                     'Новая игра! Ваш id = ' + str(maintainer.uniq_id) + '. Сообщите его участникам',
+                                     reply_markup=get_pre_start_keyboard())
+            elif message.text == participants_number:
+                bot.send_message(message.chat.id,
+                                 'Сейчас ' + str(len(maintainer.participants)) + maintainer.get_correct_ending(),
+                                 reply_markup=get_pre_start_keyboard())
+            elif message.text == start_playing:
+                bot.send_message(message.chat.id, 'Игра началась', reply_markup=get_game_keyboard())
+                Thread(target=maintainer.give_phrases(all_phrases, bot)).start()
+            elif message.text == start_game:
+                bot.send_message(message.chat.id, 'Введите id игры')
+            elif message.text == exit and maintainer is not None:
+                maintainer.is_game_started = False
+                active_maintainers.pop(maintainer.maintainer_username)
+                bot.send_message(message.chat.id, 'Игра окончена', reply_markup=get_begining_keyboard())
+            elif message.text == back:
+                bot.send_message(message.chat.id, 'Введите размер группы. Ее размер должен быть от 2 до 20',
+                                 reply_markup=get_before_game_keyboard())
+            elif message.text.isdigit():
+                if maintainer is not None and message.chat.username == maintainer.maintainer_username:
+                    if int(message.text) in range(2, 21):
+                        maintainer.group_number = int(message.text)
+                        bot.send_message(message.chat.id,
+                                         'Отлично! Размер группы ' + message.text + '. Максимальное количество участников для такого размера - ' + str(
+                                             20 * int(
+                                                 message.text)) + '. Если их будет больше, бот автоматически увеличит размер группы',
+                                         reply_markup=get_before_game_keyboard_with_back())
+                    else:
+                        bot.send_message(message.chat.id, 'Введите размер группы. Ее размер должен быть от 2 до 20',
+                                         reply_markup=get_before_game_keyboard())
+                else:
+                    maintainer = find_maintainer_by_id(int(message.text))
+                    if maintainer is not None and maintainer.is_game_started is True:
+                        maintainer.add_participant(message.chat.id)
+                        bot.send_message(message.chat.id, 'Поздравляю, ты в игре!')
+                    else:
+                        bot.send_message(message.chat.id, 'Прости, игра еще не началась :(')
 
 
-init()
-
-print('Bot is working')
-bot.polling(non_stop=True, interval=1)
+bot_polling()
